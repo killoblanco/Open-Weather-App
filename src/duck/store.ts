daeeeb30969bbc9e9299dd7b36231455fb24e5ec
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-20 17:16:19 -0500

    Update store.ts

--- src/duck/store.ts
+++ src/duck/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { weatherApi } from './api'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    [weatherApi.reducerPath]: weatherApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(weatherApi.middleware)
 })
+
+setupListeners(store.dispatch)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
