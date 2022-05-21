4bb02b1b5bfd5d143ae8f4802e4b3ace0e6f68a0
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 00:25:10 -0500

    Update globals.tsx

--- src/styles/globals.tsx
+++ src/styles/globals.tsx
@@ -1,9 +1,30 @@
-import { Global } from '@emotion/react'
+import { Global, useTheme } from '@emotion/react'
+import '@fontsource/signika/300.css'
+import '@fontsource/signika/400.css'
+import '@fontsource/signika/500.css'
+import '@fontsource/signika/600.css'
+import '@fontsource/signika/700.css'
 
 function GlobalStyles() {
-  return (
-    <Global styles={{}} />
-  )
+  const theme = useTheme()
+
+  const styles = {
+    ':root': {
+      fontFamily: theme.typography.fontFamily,
+      fontSize: theme.spacing(2)
+    },
+    '*': {
+      margin: 0,
+      padding: 0,
+      color: theme.colors.main[theme.colors.mode].onBackground
+    },
+    body: {
+      margin: 0,
+      backgroundColor: theme.colors.main[theme.colors.mode].background
+    }
+  }
+
+  return <Global styles={styles} />
 }
 
 export default GlobalStyles
