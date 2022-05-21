6a408d4fce66621f464a771898677afb52c02174
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 00:25:46 -0500

    Create text.tsx

---
+++ src/components/atoms/text.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled'
+import { createElement, HTMLProps, PropsWithChildren } from 'react'
+import { TextVariant } from '../../styles/typography'
+import { BoxProps } from './box'
+
+export interface TextProps extends BoxProps {
+  variant?: TextVariant
+}
+
+export const Text = (
+  { variant, as, children, sx, ...props }: PropsWithChildren<HTMLProps<any> & TextProps>
+) => createElement(
+  styled((as || 'p') as any)(({ theme }) => ({
+    ...theme.typography.styles[variant || 'body1'],
+    ...(sx ? (typeof sx === 'function' ? sx(theme) : sx) : {})
+  })),
+  props as any,
+  children
+)
