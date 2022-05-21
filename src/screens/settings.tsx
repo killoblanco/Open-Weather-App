cf3200bed724231b786a292fc8cd1847efe74277
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 00:25:17 -0500

    Update settings.tsx

--- src/screens/settings.tsx
+++ src/screens/settings.tsx
@@ -1,6 +1,37 @@
+import { Theme } from '@emotion/react'
+import { CSSProperties } from 'react'
+import { Box } from '../components/atoms/box'
+import { Text } from '../components/atoms/text'
+import BackBtn from '../components/buttons/back-btn'
+import ColorSwitch from '../components/settings/color-switch'
+
+const styles = {
+  root: (theme: Theme): CSSProperties => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(2),
+    padding: theme.spacing(2)
+  }),
+  row: (justify?: CSSProperties['justifyContent']) => (theme: Theme): CSSProperties => ({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: justify || 'space-between',
+    alignItems: 'center',
+    gap: theme.spacing(2)
+  })
+}
+
 function SettingsScreen() {
   return (
-    <div>Settings screen</div>
+    <Box sx={styles.root}>
+      <Box sx={styles.row('flex-start')}>
+        <BackBtn />
+        <Text variant="h4">Configuración</Text>
+      </Box>
+      <Box>
+        <ColorSwitch />
+      </Box>
+    </Box>
   )
 }
 
