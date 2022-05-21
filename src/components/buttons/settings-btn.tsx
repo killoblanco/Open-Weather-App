7ff62e4a7cb6750c6d889184ce85fba980e94f54
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 00:25:40 -0500

    Create settings-btn.tsx

---
+++ src/components/buttons/settings-btn.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled'
+import { SettingsTwoTone } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import { routes } from '../../screens/router'
+
+const Btn = styled(Link)(({ theme }) => ({
+  padding: theme.spacing(1),
+  '& svg': {
+    fontSize: theme.spacing(4)
+  }
+}))
+
+function SettingsBtn() {
+  return (
+    <Btn to={routes.settings()}>
+      <SettingsTwoTone />
+    </Btn>
+  )
+}
+
+export default SettingsBtn
