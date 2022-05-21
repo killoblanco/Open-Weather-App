b2b27ee8d6d57d37712b8c3493afc34ea9b960ba
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 00:02:20 -0500

    Update provider.tsx

--- src/styles/provider.tsx
+++ src/styles/provider.tsx
@@ -1,14 +1,53 @@
-import { ThemeProvider } from '@emotion/react'
-import { PropsWithChildren } from 'react'
+import { Theme, ThemeProvider } from '@emotion/react'
+import { createContext, PropsWithChildren, useMemo, useState } from 'react'
+import { ColorPaletteMap } from './colors'
 import GlobalStyles from './globals'
 import { theme } from './theme'
 
+interface ThemeCtxT {
+  toggleMode: () => void;
+  toggleColor: (color: keyof ColorPaletteMap) => void;
+}
+
+const ThemeCtx = createContext<ThemeCtxT>({
+  toggleMode: () => {
+  },
+  toggleColor: () => {
+  }
+})
+
 function StylesProvider({ children }: PropsWithChildren<unknown>) {
+  const [mode, setMode] = useState<'light' | 'dark'>('light')
+  const [main, setMain] = useState<keyof ColorPaletteMap>('indigo')
+
+  const nextTheme: Theme = useMemo(() => ({
+    ...theme,
+    colors: {
+      ...theme.colors,
+      mode,
+      main: theme.colors.palette[main]
+    },
+    typography: {
+      ...theme.typography,
+      fontFamily: '"Signika", sans-serif'
+    }
+  }), [mode, main])
+
+  const toggleMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light')
+  }
+
+  const toggleColor = (color: keyof ColorPaletteMap) => {
+    setMain(color)
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {children}
-    </ThemeProvider>
+    <ThemeCtx.Provider value={{ toggleColor, toggleMode }}>
+      <ThemeProvider theme={nextTheme}>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </ThemeCtx.Provider>
   )
 }
 
