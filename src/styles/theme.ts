e821bb0c42c6d07d32df49f1d49199a84254e6b5
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 00:02:15 -0500

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,16 @@
-export const theme = {}
+import { colors } from './colors'
+import { typography } from './typography'
+
+export const spacing = (all: number, leftRight?: number, bottom?: number, left?: number) => {
+  return [all, leftRight, bottom, left]
+    .filter(Boolean)
+    .map(n => n! * 8)
+    .map(n => `${n}px`)
+    .join(' ')
+}
+
+export const theme = {
+  colors,
+  spacing,
+  typography
+}
