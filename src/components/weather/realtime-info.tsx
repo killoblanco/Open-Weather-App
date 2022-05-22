39a9a03151e9d8754b9b3e7301a2ca7075b11f40
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 21:16:07 -0500

    Update realtime-info.tsx

--- src/components/weather/realtime-info.tsx
+++ src/components/weather/realtime-info.tsx
@@ -1,18 +1,14 @@
 import { Theme } from '@emotion/react'
 import { CSSProperties } from 'react'
-import { useRealtimeQuery } from '../../duck/api'
-import { useGeo } from '../../hooks/use-geo'
 import { Box } from '../atoms/box'
 import { SunkenText } from '../atoms/sunken-text'
-import { Text } from '../atoms/text'
 import SettingsBtn from '../buttons/settings-btn'
 
 const styles = {
   root: (theme: Theme): CSSProperties => ({
     display: 'flex',
     flexDirection: 'column',
-    gap: theme.spacing(2),
-    padding: theme.spacing(2)
+    gap: theme.spacing(2)
   }),
   row: (justify?: CSSProperties['justifyContent']) => (theme: Theme): CSSProperties => ({
     display: 'flex',
@@ -23,33 +19,17 @@ const styles = {
   })
 }
 
-
-function RealtimeWeatherInfo() {
-  const geo = useGeo()
-  const location = geo ? `${geo.coords.latitude},${geo.coords.longitude}` : 'auto:ip'
-  const realtime = useRealtimeQuery(location)
-
-  if (!realtime.data || realtime.isLoading) return <div>Loading...</div>
-
-  const { data } = realtime
-
+function RealtimeWeatherInfo({ weather }: { weather: ForecastQueryResponse }) {
   return (
     <Box sx={styles.root}>
       <Box sx={styles.row()}>
-        <SunkenText variant="display2">{data.location.name}</SunkenText>
+        <SunkenText variant="display2">{weather.location.name}</SunkenText>
         <SettingsBtn />
       </Box>
       <Box sx={styles.row('space-evenly')}>
-        <Box>
-          <SunkenText variant="display1">{data.current.tempC}º</SunkenText>
-        </Box>
+        <SunkenText variant="display1">{weather.current.tempC} ºC</SunkenText>
       </Box>
-      <SunkenText variant="display3">{data.current.condition.text}</SunkenText>
-      <Box sx={styles.row('space-evenly')}>
-        <Text variant="subtitle1">Humedad {data.current.humidity}%</Text>
-        <Text variant="subtitle1">Nubes {data.current.cloud}%</Text>
-        <Text variant="subtitle1">UV {data.current.uv}</Text>
-      </Box>
+      <SunkenText variant="display3">{weather.current.condition.text}</SunkenText>
     </Box>
   )
 }
