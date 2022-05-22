7cb12644cf19e274270a03449acf4538ad294e1c
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 23:47:12 -0500

    Update sun-rise-set.tsx

--- src/components/weather/cards/sun-rise-set.tsx
+++ src/components/weather/cards/sun-rise-set.tsx
@@ -1,5 +1,6 @@
 import { Theme } from '@emotion/react'
 import { BedTwoTone, FreeBreakfastTwoTone } from '@mui/icons-material'
+import { CSSProperties } from 'react'
 import { Box } from '../../atoms/box'
 import { Card } from '../../atoms/card'
 import { Text } from '../../atoms/text'
@@ -9,21 +10,30 @@ const styles = {
   row: (theme: Theme) => ({
     display: 'flex',
     gap: theme.spacing(1)
+  }),
+  group: (theme: Theme): CSSProperties => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(1.25)
   })
 }
 
 function SunRiseSetCard({ astro }: { astro: WeatherApiState['astronomy'] }) {
   return (
     <Card sx={styles.root}>
-      <Box sx={styles.row}>
-        <FreeBreakfastTwoTone fontSize="small" />
-        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>amanecer</Text>
+      <Box sx={styles.group}>
+        <Box sx={styles.row}>
+          <FreeBreakfastTwoTone fontSize="small" />
+          <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>amanecer</Text>
+        </Box>
+        <Text variant="h2">{astro.sunrise}</Text>
       </Box>
-      <Text variant="h2">{astro.sunrise}</Text>
-      <Text variant="h4">{astro.sunset}</Text>
-      <Box sx={styles.row}>
-        <BedTwoTone fontSize="small" />
-        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>atardecer</Text>
+      <Box sx={styles.group}>
+        <Text variant="h4">{astro.sunset}</Text>
+        <Box sx={styles.row}>
+          <BedTwoTone fontSize="small" />
+          <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>atardecer</Text>
+        </Box>
       </Box>
     </Card>
   )
