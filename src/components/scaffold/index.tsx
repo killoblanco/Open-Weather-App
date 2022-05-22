436ef47a3311bff182e7453753d3b74e44482843
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 21:16:35 -0500

    Update index.tsx

--- src/components/scaffold/index.tsx
+++ src/components/scaffold/index.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from 'react'
+import { Box } from '../atoms/box'
 
-function Scaffold({children}:PropsWithChildren<unknown>) {
+function Scaffold({ children }: PropsWithChildren<unknown>) {
   return (
-    <div>
+    <Box sx={(theme) => ({ padding: theme.spacing(2) })}>
       {children}
-    </div>
+    </Box>
   )
 }
 
