301cc0d35102723f2e881171547cb380fc354e2b
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-21 21:16:00 -0500

    Update types.d.ts

--- src/duck/api/types.d.ts
+++ src/duck/api/types.d.ts
@@ -1,3 +1,9 @@
+interface WeatherCondition {
+  text: string;
+  icon: string;
+  code: number;
+}
+
 interface WeatherApiState {
   search: {
     id: number;
@@ -24,11 +30,7 @@ interface WeatherApiState {
     tempC: number;
     tempF: number;
     isDay: number;
-    condition: {
-      text: string;
-      icon: string;
-      code: number;
-    };
+    condition: WeatherCondition;
     windMph: number;
     windKph: number;
     windDegree: number;
@@ -55,4 +57,37 @@ interface WeatherApiState {
     moonPhase: string;
     moonIllumination: string;
   };
+  forecast: {
+    date: string;
+    dateEpoch: number;
+    day: {
+      maxtempC:number;
+      maxtempF:number;
+      mintempC:number;
+      mintempF:number;
+      avgtempC:number;
+      avgtempF:number;
+      maxwindMph:number;
+      maxwindKph:number;
+      totalprecipMm:number;
+      totalprecipIn:number;
+      avgvisKm:number;
+      avgvisMiles:number;
+      avghumidity:number;
+      dailyWillItRain:number;
+      dailyChanceOfRain:number;
+      dailyWillItSnow:number;
+      dailyChanceOfSnow:number;
+      condition: WeatherCondition
+      uv:number;
+    }
+    astro: WeatherApiState['astronomy']
+    hour: {}[]
+  }[]
+}
+
+interface ForecastQueryResponse {
+  location: WeatherApiState['location'],
+  current: WeatherApiState['current'],
+  forecast: WeatherApiState['forecast']
 }
