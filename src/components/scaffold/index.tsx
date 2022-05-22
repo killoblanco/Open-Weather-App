7ea6ec94707a49dc0401985798c9a3632425402f
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-22 02:07:31 -0500

    Update index.tsx

--- src/components/scaffold/index.tsx
+++ src/components/scaffold/index.tsx
@@ -1,9 +1,19 @@
-import { PropsWithChildren } from 'react'
+import { Theme } from '@emotion/react'
+import { CSSProperties, PropsWithChildren } from 'react'
 import { Box } from '../atoms/box'
+
+const styles = {
+  root: (theme: Theme): CSSProperties => ({
+    margin: '0 auto',
+    maxWidth: theme.spacing(55),
+    padding: theme.spacing(2),
+  })
+}
+
 
 function Scaffold({ children }: PropsWithChildren<unknown>) {
   return (
-    <Box sx={(theme) => ({ padding: theme.spacing(2) })}>
+    <Box sx={styles.root}>
       {children}
     </Box>
   )
