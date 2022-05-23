1c153379e68fb56233b03c16b566be02ba467a5f
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-23 01:12:06 -0500

    Update visibility.tsx

--- src/components/weather/cards/visibility.tsx
+++ src/components/weather/cards/visibility.tsx
@@ -1,5 +1,6 @@
 import { Theme } from '@emotion/react'
 import { VisibilityTwoTone } from '@mui/icons-material'
+import { useTranslation } from 'react-i18next'
 import { Box } from '../../atoms/box'
 import { Card } from '../../atoms/card'
 import { Text } from '../../atoms/text'
@@ -13,11 +14,14 @@ const styles = {
 }
 
 function VisibilityCard({ current }: { current: WeatherApiState['current'] }) {
+  const { t } = useTranslation()
   return (
     <Card sx={styles.root}>
       <Box sx={styles.row}>
         <VisibilityTwoTone fontSize="small" />
-        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>visibilidad</Text>
+        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>
+          {t('weather.visibility')}
+        </Text>
       </Box>
       <Text variant="display2">{current.visKm} Km</Text>
     </Card>
