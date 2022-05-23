1466786144a888d783302be96cb07bc240884ceb
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-22 21:44:19 -0500

    Create list-item.tsx

---
+++ src/components/atoms/list-item.tsx
@@ -0,0 +1,43 @@
+import { Theme } from '@emotion/react'
+import { CSSProperties } from 'react'
+import { Box } from './box'
+import { Text } from './text'
+
+interface ListItemProps {
+  icon: JSX.Element;
+  onClick?: () => void;
+  subtitle?: string;
+  title: string;
+  trailing?: JSX.Element;
+}
+
+const styles = {
+  root: (theme: Theme): any => ({
+    display: 'flex',
+    gap: theme.spacing(2),
+    padding: theme.spacing(2, 0),
+    '&:not(:last-of-type)': {
+      borderBottom: `1px solid ${theme.colors.main[theme.colors.mode].outline}44`
+    }
+  }),
+  text: {
+    display: 'flex',
+    flex: 1,
+    gap: 0
+  }
+}
+
+function ListItem({ icon, onClick, subtitle, title, trailing }: ListItemProps) {
+  return (
+    <Box onClick={onClick} role="presentation" sx={styles.root}>
+      {icon}
+      <Box sx={styles.text}>
+        <Text variant="subtitle1">{title}</Text>
+        {subtitle ? <Text variant="subtitle2">{subtitle}</Text> : null}
+      </Box>
+      {trailing}
+    </Box>
+  )
+}
+
+export default ListItem
