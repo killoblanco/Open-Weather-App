1a369a2c6b5cd1ca63a8598e40c345abcd28c004
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-23 01:12:41 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
+import { I18nextProvider } from 'react-i18next'
 import { Provider } from 'react-redux'
 import Scaffold from './components/scaffold'
 import { store } from './duck/store'
+import i18n from './locales'
 import Router from './screens/router'
 import StylesProvider from './styles/provider'
 
@@ -9,11 +11,13 @@ function App() {
   return (
     <StrictMode>
       <Provider store={store}>
-        <StylesProvider>
-          <Scaffold>
-            <Router />
-          </Scaffold>
-        </StylesProvider>
+        <I18nextProvider i18n={i18n}>
+          <StylesProvider>
+            <Scaffold>
+              <Router />
+            </Scaffold>
+          </StylesProvider>
+        </I18nextProvider>
       </Provider>
     </StrictMode>
   )
