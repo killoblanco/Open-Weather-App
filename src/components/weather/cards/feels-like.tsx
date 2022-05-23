c119c7f557da4e052a66be8e6c8a5edee56a025a
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-23 01:12:25 -0500

    Update feels-like.tsx

--- src/components/weather/cards/feels-like.tsx
+++ src/components/weather/cards/feels-like.tsx
@@ -1,5 +1,6 @@
 import { Theme } from '@emotion/react'
 import { ThermostatTwoTone } from '@mui/icons-material'
+import { useTranslation } from 'react-i18next'
 import { Box } from '../../atoms/box'
 import { Card } from '../../atoms/card'
 import { Text } from '../../atoms/text'
@@ -13,11 +14,14 @@ const styles = {
 }
 
 function FeelsLikeCard({ current }: { current: WeatherApiState['current'] }) {
+  const { t } = useTranslation()
   return (
     <Card sx={styles.root}>
       <Box sx={styles.row}>
         <ThermostatTwoTone fontSize="small" />
-        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>sensación</Text>
+        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>
+          {t('weather.feelsLike')}
+        </Text>
       </Box>
       <Text variant="display1">{current.feelslikeC.toFixed(0)}º</Text>
     </Card>
