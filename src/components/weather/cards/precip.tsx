afd7ef3dd92e1bf4b5dbf588e85ef7af713d7230
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-23 01:12:19 -0500

    Update precip.tsx

--- src/components/weather/cards/precip.tsx
+++ src/components/weather/cards/precip.tsx
@@ -1,5 +1,6 @@
 import { Theme } from '@emotion/react'
 import { OpacityTwoTone } from '@mui/icons-material'
+import { useTranslation } from 'react-i18next'
 import { Box } from '../../atoms/box'
 import { Card } from '../../atoms/card'
 import { Text } from '../../atoms/text'
@@ -13,11 +14,14 @@ const styles = {
 }
 
 function PrecipCard({ current }: { current: WeatherApiState['current'] }) {
+  const { t } = useTranslation()
   return (
     <Card sx={styles.root}>
       <Box sx={styles.row}>
         <OpacityTwoTone fontSize="small" />
-        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>precipitación</Text>
+        <Text variant="subtitle2" sx={{ textTransform: 'uppercase' }}>
+          {t('weather.precip')}
+        </Text>
       </Box>
       <Text variant="display3">{current.precipMm} mm</Text>
     </Card>
