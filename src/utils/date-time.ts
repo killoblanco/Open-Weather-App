d2607e46f18087843f2bac7ae7f4e509d646dc98
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-22 21:43:04 -0500

    Update date-time.ts

--- src/utils/date-time.ts
+++ src/utils/date-time.ts
@@ -1,3 +1,14 @@
 export const parse24hTime = (dateTime: string, locale: string = 'es-CO') => new Intl
-  .DateTimeFormat(locale, {timeStyle: 'short'})
+  .DateTimeFormat(locale, { timeStyle: 'short' })
   .format(new Date(dateTime))
+
+export const parseShortDate = (dateTime: string, locale: string = 'es-CO') => new Intl
+  .DateTimeFormat(locale, { dateStyle: 'short' })
+  .format(new Date(dateTime))
+
+export const parseRelativeTime = (dateTime: string, locale: string = 'es-CO') => new Intl
+  .RelativeTimeFormat(locale, { numeric: 'auto' })
+  .format(
+    new Date(dateTime.replaceAll('-', '/')).getDate() - new Date().getDate(),
+    'day'
+  )
