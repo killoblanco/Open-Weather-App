6442921c8b1b32e5f881e0f71933eca7ab360a9d
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-22 22:38:55 -0500

    Update card.tsx

--- src/components/atoms/card.tsx
+++ src/components/atoms/card.tsx
@@ -3,12 +3,12 @@ import { Box } from './box'
 
 export const Card = styled(Box)(({ theme }) => ({
   aspectRatio: '1 / 1',
-  backgroundColor: theme.colors.main[theme.colors.mode].primaryContainer,
+  backgroundColor: theme.colors.main[theme.colors.mode].secondaryContainer,
   backdropFilter: `blur(${theme.spacing(0.5)})`,
   borderRadius: theme.borderRadius,
   display: 'flex',
   flexDirection: 'column',
   padding: theme.spacing(1.5),
   width: theme.spacing(19.8),
-  '& *': { color: theme.colors.main[theme.colors.mode].onPrimaryContainer }
+  '& *': { color: theme.colors.main[theme.colors.mode].onSecondaryContainer }
 }))
