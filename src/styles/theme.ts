b376ee6bc55d9b7db18724e11fe76910e09f96bc
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-22 21:43:09 -0500

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ import { typography } from './typography'
 
 export const spacing = (all: number, leftRight?: number, bottom?: number, left?: number) => {
   return [all, leftRight, bottom, left]
-    .filter(Boolean)
+    .filter(n => n === 0 ? true : Boolean(n))
     .map(n => n! * 8)
     .map(n => `${n}px`)
     .join(' ')
@@ -18,8 +18,8 @@ export const theme = {
     duration: {
       quick: '200ms',
       fast: '250ms',
-      normal: '300ms',
+      normal: '300ms'
     }
   },
-  typography,
+  typography
 }
