26db7e61c4bd41d2133c6acf8bf95b0422fb5b1e
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-23 01:10:50 -0500

    Update typography.ts

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,5 +1,5 @@
 export interface TypographyOptions {
-  fontWeight: 'medium' | 'normal' | 'bold' | 'bolder' | 'lighter' | 'initial' | 'inherit' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
+  fontWeight: 500 | 400 | 'bold' | 'bolder' | 'lighter' | 'initial' | 'inherit' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
   fontSize: number;
   lineHeight: string;
   letterSpacing: number;
@@ -32,97 +32,97 @@ export const typography: Typography = {
   fontFamily: 'sans-serif',
   styles: {
     display1: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 64,
       lineHeight: '76px',
       letterSpacing: -0.5,
     },
     display2: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 57,
       lineHeight: '64px',
       letterSpacing: -0.25,
     },
     display3: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 45,
       lineHeight: '52px',
       letterSpacing: 0,
     },
     h1: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 36,
       lineHeight: '44px',
       letterSpacing: 0,
     },
     h2: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 32,
       lineHeight: '40px',
       letterSpacing: 0,
     },
     h3: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 28,
       lineHeight: '36px',
       letterSpacing: 0,
     },
     h4: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 24,
       lineHeight: '32px',
       letterSpacing: 0,
     },
     h5: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 22,
       lineHeight: '28px',
       letterSpacing: 0,
     },
     h6: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 18,
       lineHeight: '24px',
       letterSpacing: 0,
     },
     subtitle1: {
-      fontWeight: 'medium',
+      fontWeight: 500,
       fontSize: 16,
       lineHeight: '24px',
       letterSpacing: 0.1,
     },
     subtitle2: {
-      fontWeight: 'medium',
+      fontWeight: 500,
       fontSize: 14,
       lineHeight: '20px',
       letterSpacing: 0.1,
     },
     body1: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 16,
       lineHeight: '24px',
       letterSpacing: 0.5,
     },
     body2: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 14,
       lineHeight: '20px',
       letterSpacing: 0.25,
     },
     button: {
-      fontWeight: 'medium',
+      fontWeight: 500,
       fontSize: 14,
       lineHeight: '20px',
       letterSpacing: 0.1,
     },
     caption: {
-      fontWeight: 'normal',
+      fontWeight: 400,
       fontSize: 12,
       lineHeight: '16px',
       letterSpacing: 0.4,
     },
     overline: {
-      fontWeight: 'medium',
+      fontWeight: 500,
       fontSize: 12,
       lineHeight: '16px',
       letterSpacing: 0.5,
