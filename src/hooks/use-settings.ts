7c147b28479c7696b8cc218eb0e7545bdf4e3f0d
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-23 19:57:53 -0500

    Update use-settings.ts

--- src/hooks/use-settings.ts
+++ src/hooks/use-settings.ts
@@ -1,14 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { settingsActions, settingsSelectors } from '../duck/settings'
 import { AppDispatch } from '../duck/store'
-import { colors } from '../styles/colors'
+import { ColorPaletteMap, colors } from '../styles/colors'
 
 export const useSettings = () => {
   const state = useSelector(settingsSelectors)
   const dispatch = useDispatch<AppDispatch>()
 
   const actions = {
-    setThemeColor: (color: keyof typeof colors.palette) => dispatch(settingsActions.setThemeColor(color)),
+    setThemeColor: (color: keyof ColorPaletteMap) => dispatch(settingsActions.setThemeColor(color)),
     setDarkMode: (darkMode: boolean) => dispatch(settingsActions.setDarkMode(darkMode)),
     setDegMeasure: (degMeasure: 'c' | 'f') => dispatch(settingsActions.setDegMeasure(degMeasure)),
     setLang: (lang: string) => dispatch(settingsActions.setLang(lang))
