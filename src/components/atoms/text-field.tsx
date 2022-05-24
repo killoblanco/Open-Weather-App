c882a696e6a29b63893dfb04405dc635841991bc
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-24 01:06:42 -0500

    Create text-field.tsx

---
+++ src/components/atoms/text-field.tsx
@@ -0,0 +1,44 @@
+import { Theme } from '@emotion/react'
+import styled from '@emotion/styled'
+import { nanoid } from '@reduxjs/toolkit'
+import { ChangeEvent, CSSProperties } from 'react'
+import { Box } from './box'
+import { Text } from './text'
+
+interface TextFieldProps {
+  label: string
+  value: string
+  onChange: (value: ChangeEvent<HTMLInputElement>) => void
+}
+
+const styles = {
+  root: (theme: Theme): CSSProperties => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.spacing(1)
+  }),
+  label: (theme: Theme): CSSProperties => ({ paddingLeft: theme.spacing(2) })
+}
+
+const Input = styled.input(({ theme }) => ({
+  backgroundColor: theme.colors.main[theme.colors.mode].secondaryContainer,
+  border: 'none',
+  borderRadius: theme.borderRadius,
+  color: theme.colors.main[theme.colors.mode].onSecondaryContainer,
+  outline: 'none',
+  padding: theme.spacing(2),
+  ...theme.typography.styles.body1
+}))
+
+export function TextField({ label, value, onChange }: TextFieldProps) {
+  const id = nanoid(6)
+
+  return (
+    <Box sx={styles.root}>
+      <Text as="label" variant="subtitle1" htmlFor={id} sx={styles.label}>
+        {label}
+      </Text>
+      <Input type="text" id={id} value={value} onChange={onChange} />
+    </Box>
+  )
+}
