d15caa268457218f91ad7304957a62f596dd6971
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-23 23:18:57 -0500

    Update backdrop.ts

--- src/components/scaffold/backdrop.ts
+++ src/components/scaffold/backdrop.ts
@@ -1,15 +1,17 @@
 import styled from '@emotion/styled'
+import { CSSProperties } from 'react'
 
-export const Backdrop = styled.div(({ theme }) => ({
+export const Backdrop = styled.div<{ justify?: CSSProperties['justifyContent'] }>(({ theme, justify }) => ({
   alignItems: 'center',
   backdropFilter: `blur(${theme.spacing(0.5)})`,
   backgroundColor: `${theme.colors.main[theme.colors.mode].surface}11`,
   display: 'flex',
   flexDirection: 'column-reverse',
   height: `calc(100vh - ${theme.spacing(8)})`,
+  justifyContent: justify,
   left: 0,
   padding: theme.spacing(4),
   position: 'fixed',
   top: 0,
-  width: `calc(100vw - ${theme.spacing(8)})`,
+  width: `calc(100vw - ${theme.spacing(8)})`
 }))
