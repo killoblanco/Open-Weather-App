e2adaa08d70a10783cdae51b867f05eccb18a1d5
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-24 01:06:30 -0500

    Update index.tsx

--- src/components/scaffold/index.tsx
+++ src/components/scaffold/index.tsx
@@ -9,9 +9,9 @@ import { Dialogs } from './dialogs'
 const styles = {
   root: (theme: Theme): CSSProperties => ({
     margin: '0 auto',
-    padding: theme.spacing(2, 2, 4),
+    padding: theme.spacing(2, 2, 0),
     width: `calc(100vw - ${theme.spacing(4)})`,
-    height: `calc(100vh - ${theme.spacing(4)})`,
+    height: `calc(100vh - ${theme.spacing(2)})`,
     overflow: 'hidden'
   })
 }
