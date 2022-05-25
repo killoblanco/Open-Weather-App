111675a5aace8a79f5daef141e6ef2c9acd79033
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-24 19:36:45 -0500

    Update locations-list.tsx

--- src/components/dialogs/locations-list.tsx
+++ src/components/dialogs/locations-list.tsx
@@ -1,9 +1,11 @@
 import { Theme } from '@emotion/react'
+import { DeleteTwoTone } from '@mui/icons-material'
 import { nanoid } from '@reduxjs/toolkit'
 import { CSSProperties } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLocations } from '../../hooks/use-locations'
 import { Box } from '../atoms/box'
+import { IconBtn } from '../atoms/icon-btn'
 import ListItem from '../atoms/list-item'
 import { useDialogs } from '../scaffold/context'
 
@@ -16,7 +18,7 @@ const styles = {
 function LocationsListDialog() {
   const { t } = useTranslation()
   const { state: { props }, actions: { close } } = useDialogs()
-  const { state } = useLocations()
+  const { state, actions: { removeLocation } } = useLocations()
 
   const handleClick = (loc: string) => {
     if (props.onClick) props.onClick(loc)
@@ -28,11 +30,17 @@ function LocationsListDialog() {
 
   return (
     <Box sx={styles.root}>
-      {locations.map(({ value, label }) => (
+      {locations.map(({ value, label }, idx) => (
         <ListItem
           key={nanoid(6)}
           onClick={() => handleClick(value)}
           title={label}
+          trailing={(idx > 0
+              ? <IconBtn onClick={() => removeLocation(value)}>
+                <DeleteTwoTone />
+              </IconBtn>
+              : undefined
+          )}
         />
       ))}
     </Box>
