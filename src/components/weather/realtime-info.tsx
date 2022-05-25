08fcdedf207f0f0f4dc4804e20d410fb55c65d2f
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-24 20:38:57 -0500

    Update realtime-info.tsx

--- src/components/weather/realtime-info.tsx
+++ src/components/weather/realtime-info.tsx
@@ -1,41 +1,60 @@
 import { Theme } from '@emotion/react'
-import { CSSProperties } from 'react'
+import { CSSProperties, useEffect, useState } from 'react'
 import { useSettings } from '../../hooks/use-settings'
 import { Box } from '../atoms/box'
 import { SunkenText } from '../atoms/sunken-text'
 import SettingsBtn from '../buttons/settings-btn'
-import WeatherIcon from './icon'
+import { getIconPath } from './icon'
 
 const styles = {
-  root: (theme: Theme): CSSProperties => ({
+  root: (icon: string) => (theme: Theme): any => ({
     display: 'flex',
     flexDirection: 'column',
-    gap: theme.spacing(2),
+    backgroundImage: `url("${icon}")`,
+    backgroundSize: 'contain',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: `${theme.spacing(-3)} center`,
+    gap: theme.spacing(8),
     maxWidth: theme.spacing(70),
-    width: '100%'
+    alignItems: 'center',
+    width: '100%',
   }),
-  row: (justify?: CSSProperties['justifyContent']) => (theme: Theme): CSSProperties => ({
+  row: (theme: Theme): CSSProperties => ({
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: justify || 'space-between',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    width: '100%',
     gap: theme.spacing(0)
+  }),
+  text: (theme: Theme): any => ({
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+    width: '100%',
+    alignItems: 'center',
+    gap: theme.spacing(0),
+    '& > p': { fontSize: theme.spacing(12) }
   })
 }
 
 function RealtimeWeatherInfo({ weather }: { weather: ForecastQueryResponse }) {
   const { state: { degMeasure } } = useSettings()
+  const [icon, setIcon] = useState('')
 
   const currentTemp = degMeasure === 'c' ? weather.current.tempC : weather.current.tempF
 
+  useEffect(() => {
+    getIconPath(weather.current.condition.code, !!weather.current.isDay).then(setIcon)
+  }, [weather])
+
   return (
-    <Box sx={styles.root}>
-      <Box sx={styles.row()}>
+    <Box sx={styles.root(icon)}>
+      <Box sx={styles.row}>
         <SunkenText variant="display3">{weather.location.name}</SunkenText>
         <SettingsBtn />
       </Box>
-      <Box sx={styles.row('center')}>
-        <WeatherIcon code={weather.current.condition.code} isDay={!!weather.current.isDay} />
+      <Box sx={styles.text}>
         <SunkenText variant="display1">{currentTemp}º</SunkenText>
       </Box>
       <SunkenText variant="h1">{weather.current.condition.text}</SunkenText>
