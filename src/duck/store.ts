2c409f4e573bcb5306bbd89ce1c7bdad7118042a
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-24 19:36:23 -0500

    Update store.ts

--- src/duck/store.ts
+++ src/duck/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { weatherApi } from './api'
+import { getLocalStorage, localStorageMiddleware } from './local-storage-middleware'
 import { locationsReducer } from './locations'
 import { settingsReducer } from './settings'
 
 export const store = configureStore({
+  preloadedState: getLocalStorage(),
   reducer: {
     locations: locationsReducer,
     settings: settingsReducer,
     [weatherApi.reducerPath]: weatherApi.reducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(localStorageMiddleware)
     .concat(weatherApi.middleware)
 })
 
