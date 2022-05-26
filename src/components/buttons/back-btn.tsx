e06c19641e1b1d210317a4493ef2ad49343f192d
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-25 21:17:20 -0500

    Update back-btn.tsx

--- src/components/buttons/back-btn.tsx
+++ src/components/buttons/back-btn.tsx
@@ -1,14 +1,14 @@
-import { ChevronLeftTwoTone } from '@mui/icons-material'
-import { Link } from 'react-router-dom'
-import { routes } from '../../screens/router'
-import { IconBtn } from '../atoms/icon-btn'
+import { ChevronLeftTwoTone } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { routes } from '../../screens/router';
+import IconBtn from '../atoms/icon-btn';
 
 function BackBtn() {
   return (
     <IconBtn as={Link} to={routes.main()}>
       <ChevronLeftTwoTone />
     </IconBtn>
-  )
+  );
 }
 
-export default BackBtn
+export default BackBtn;
