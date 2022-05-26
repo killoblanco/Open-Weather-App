af8e2ef9f2a230998a20e608925799a095b39e7c
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-25 21:19:51 -0500

    Update use-locations.ts

--- src/hooks/use-locations.ts
+++ src/hooks/use-locations.ts
@@ -2,14 +2,16 @@ import { useDispatch, useSelector } from 'react-redux'
 import { locationsActions, locationsSelectors } from '../duck/locations'
 import { AppDispatch } from '../duck/store'
 
-export const useLocations = () => {
+const useLocations = () => {
   const dispatch = useDispatch<AppDispatch>()
   const state = useSelector(locationsSelectors)
 
   const actions = {
     addLocation: (location: string) => dispatch(locationsActions.addLocation(location)),
-    removeLocation: (location: string) => dispatch(locationsActions.removeLocation(location)),
+    removeLocation: (location: string) => dispatch(locationsActions.removeLocation(location))
   }
 
   return { state, actions }
 }
+
+export default useLocations
