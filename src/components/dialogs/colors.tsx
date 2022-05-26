2b4739879d799b943f16caa5bba166a7f9d0e9d3
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-25 21:17:37 -0500

    Update colors.tsx

--- src/components/dialogs/colors.tsx
+++ src/components/dialogs/colors.tsx
@@ -1,12 +1,12 @@
-import { Theme, useTheme } from '@emotion/react'
-import { nanoid } from '@reduxjs/toolkit'
-import { CSSProperties, useContext, useMemo } from 'react'
-import { useTranslation } from 'react-i18next'
-import { useSettings } from '../../hooks/use-settings'
-import { ColorPaletteMap } from '../../styles/colors'
-import { Box } from '../atoms/box'
-import ListItem from '../atoms/list-item'
-import { ScaffoldCtx } from '../scaffold/context'
+import { Theme, useTheme } from '@emotion/react';
+import { nanoid } from '@reduxjs/toolkit';
+import { CSSProperties, useContext, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import useSettings from '../../hooks/use-settings';
+import { ColorPaletteMap } from '../../styles/colors';
+import { Box } from '../atoms/box';
+import ListItem from '../atoms/list-item';
+import { ScaffoldCtx } from '../scaffold/context';
 
 const styles = {
   root: (theme: Theme): CSSProperties => ({
@@ -19,26 +19,27 @@ const styles = {
     width: theme.spacing(4),
     height: theme.spacing(4),
     borderRadius: '50%',
-    backgroundColor: color
-  })
-}
+    backgroundColor: color,
+  }),
+};
 
 function LangDialog() {
-  const { t } = useTranslation()
-  const { actions: { setThemeColor } } = useSettings()
-  const { dialogs: { actions: { close } } } = useContext(ScaffoldCtx)
-  const { colors: { mode, palette } } = useTheme()
+  const { t } = useTranslation();
+  const { actions: { setThemeColor } } = useSettings();
+  const { dialogs: { actions: { close } } } = useContext(ScaffoldCtx);
+  const { colors: { mode, palette } } = useTheme();
 
   const colors = useMemo(
     () => Object.entries(palette)
       .map(([name, color]) => ({ [name]: color[mode].primary }))
       .reduce((acc, cur) => ({ ...acc, ...cur }), {}),
-    [mode, palette])
+    [mode, palette],
+  );
 
   const handleClose = (color: string) => {
-    setThemeColor(color as keyof ColorPaletteMap)
-    close()
-  }
+    setThemeColor(color as keyof ColorPaletteMap);
+    close();
+  };
 
   return (
     <Box sx={styles.root}>
@@ -51,7 +52,7 @@ function LangDialog() {
         />
       ))}
     </Box>
-  )
+  );
 }
 
-export default LangDialog
+export default LangDialog;
