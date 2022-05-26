81ba81a3d4b2b9cd3dd0dd581cce7b7c62c5f27e
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-25 21:19:35 -0500

    Update main.tsx

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -2,8 +2,8 @@ import { nanoid } from '@reduxjs/toolkit'
 import { useMemo } from 'react'
 import LocationWeatherLayout from '../components/layout/location-weather'
 import Slider from '../components/layout/slider'
-import { useGeo } from '../hooks/use-geo'
-import { useLocations } from '../hooks/use-locations'
+import useGeo from '../hooks/use-geo'
+import useLocations from '../hooks/use-locations'
 
 function MainScreen() {
   useGeo()
@@ -13,7 +13,9 @@ function MainScreen() {
   const slides = useMemo(() => ({
     [state.geo]: <LocationWeatherLayout key={nanoid()} location={state.geo} />,
     ...state.list
-      .map(location => ({ [location]: <LocationWeatherLayout key={nanoid()} location={location} /> }))
+      .map((location) => ({
+        [location]: <LocationWeatherLayout key={nanoid()} location={location} />
+      }))
       .reduce((acc, cur) => ({ ...acc, ...cur }), {})
   }), [state.geo, state.list])
 
