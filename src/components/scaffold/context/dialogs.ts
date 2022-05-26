942b7bf081d43c815ee82e1af0d5b62361c01b9f
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-25 21:21:42 -0500

    Update dialogs.ts

--- src/components/scaffold/context/dialogs.ts
+++ src/components/scaffold/context/dialogs.ts
@@ -4,19 +4,21 @@ export const dialogsInitialState = {
   open: false,
   title: '',
   content: '',
-  props: {} as any,
+  // eslint-disable-next-line no-unused-vars
+  props: {} as { onClick?: (d: any) => void }
 }
 
 export const dialogsDuck = createSlice({
   name: 'dialogs',
   initialState: dialogsInitialState,
   reducers: {
-    openDialog: (state, action: PayloadAction<Partial<typeof dialogsInitialState>>) => {
-      state.open = true
-      state.title = action.payload.title!
-      state.content = action.payload.content!
-      state.props = action.payload.props || {}
-    },
+    openDialog: (state, { payload }: PayloadAction<Partial<typeof dialogsInitialState>>) => ({
+      ...state,
+      open: true,
+      title: payload.title || '',
+      content: payload.content || '',
+      props: payload.props || {}
+    }),
     closeDialog: () => dialogsInitialState
   }
 })
