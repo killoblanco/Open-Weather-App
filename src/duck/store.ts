6db4bfa11b541fa2693f3ca212f65ed1943490e7
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-25 21:19:57 -0500

    Update store.ts

--- src/duck/store.ts
+++ src/duck/store.ts
@@ -1,23 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { weatherApi } from './api'
-import { getLocalStorage, localStorageMiddleware } from './local-storage-middleware'
-import { locationsReducer } from './locations'
-import { settingsReducer } from './settings'
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { weatherApi } from './api';
+import { getLocalStorage, localStorageMiddleware } from './local-storage-middleware';
+import { locationsReducer } from './locations';
+import { settingsReducer } from './settings';
 
 export const store = configureStore({
   preloadedState: getLocalStorage(),
   reducer: {
     locations: locationsReducer,
     settings: settingsReducer,
-    [weatherApi.reducerPath]: weatherApi.reducer
+    [weatherApi.reducerPath]: weatherApi.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware()
     .concat(localStorageMiddleware)
-    .concat(weatherApi.middleware)
-})
+    .concat(weatherApi.middleware),
+});
 
-setupListeners(store.dispatch)
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
