81f62aff485a9f3fc8c436600a42273b4075ad0f
Kamilo Vasquez <killo.blanco@gmail.com>
2022-05-25 21:20:51 -0500

    Update uv.tsx

--- src/components/weather/meters/uv.tsx
+++ src/components/weather/meters/uv.tsx
@@ -1,6 +1,6 @@
-import { Theme } from '@emotion/react'
-import { CSSProperties } from 'react'
-import { Box } from '../../atoms/box'
+import { Theme } from '@emotion/react';
+import { CSSProperties } from 'react';
+import { Box } from '../../atoms/box';
 
 const styles = {
   root: (theme: Theme): CSSProperties => ({
@@ -16,7 +16,7 @@ const styles = {
     height: theme.spacing(1.5),
     overflow: 'hidden',
     position: 'relative',
-    width: '100%'
+    width: '100%',
   }),
   cursor: (position: number) => (theme: Theme): CSSProperties => ({
     borderRadius: theme.borderRadius,
@@ -28,16 +28,16 @@ const styles = {
     transition: `all ${theme.transitions.duration.normal} ${theme.transitions.timing}`,
     top: -1,
     width: theme.spacing(0.25),
-  })
-}
+  }),
+};
 
 function UvMeter({ uv }: { uv: number }) {
-  const position = uv >= 11 ? 97 : uv * 9
+  const position = uv >= 11 ? 97 : uv * 9;
   return (
     <Box sx={styles.root}>
       <Box sx={styles.cursor(position)} />
     </Box>
-  )
+  );
 }
 
-export default UvMeter
+export default UvMeter;
